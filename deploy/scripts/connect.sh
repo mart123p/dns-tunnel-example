dc2637427bd48853745740fa8e24165575866266
Martin Pouliot <martinp507@gmail.com>
2020-11-09 11:00:52 -0500

    Fixed IP addresses

--- deploy/scripts/connect.sh
+++ deploy/scripts/connect.sh
@@ -2,12 +2,12 @@
 
 # Setup the port forwarding from ssh connexion for remote server
 ip=$(cat ../creds/ip)
-ssh -L 6001:10.0.0.3:5986 \
-    -L 6002:10.0.0.4:5986 \
+ssh -L 6001:10.0.0.4:5986 \
+    -L 6002:10.0.0.5:5986 \
     \
-    -L 6011:10.0.0.3:3389 \
-    -L 6012:10.0.0.4:3389 \
+    -L 6011:10.0.0.4:3389 \
+    -L 6012:10.0.0.5:3389 \
     \
-    -L 6050:10.0.5.2:22 \
+    -L 6050:10.0.5.4:22 \
     -i ../creds/ssh/id_rsa \
     azureuser@$ip
