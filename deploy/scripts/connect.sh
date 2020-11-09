a2d96a8b1a22a8f47239876548221f0b389f28ae
Martin Pouliot <martinp507@gmail.com>
2020-11-08 22:56:47 -0500

    Added connect.sh script

---
+++ deploy/scripts/connect.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Setup the port forwarding from ssh connexion for remote server
+ip=$(cat ../creds/ip)
+ssh -L 6001:10.0.0.3:5986 \
+    -L 6002:10.0.0.4:5986 \
+    \
+    -L 6011:10.0.0.3:3389 \
+    -L 6012:10.0.0.4:3389 \
+    \
+    -L 6050:10.0.5.2:22 \
+    azureuser@$ip
